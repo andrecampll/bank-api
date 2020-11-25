dcb46dbcf7325f9c1e9ab96b5688a97b5e3364a9
André <andrevictor50@gmail.com>
2020-11-25 11:40:16 -0300

    defining user model and migration

--- lib/bank_api/schemas/user.ex
+++ lib/bank_api/schemas/user.ex
@@ -2,11 +2,15 @@ defmodule BankApi.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_key, autogenerate: true}
+  @derive {Phoenix.Param, key: :id}
   schema "users" do
-    field(:first_name, :string)
-    field(:last_name, :string)
-    field(:email, :string)
-    field(:age, :integer)
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :role, :string
+
     timestamps()
   end
  end
